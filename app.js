dfd4f84d75940792e98b6eedb47c7383955e671d
Sherin Binu <sherinbinu@hotmail.com>
2017-05-16 10:20:43 +0530

    implemented abstraction

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
-const request = require('request');
+const yargs = require('yargs');
 
+const geocode = require('./geocode/geocode');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: "Address of the location",
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+geocode.geocodeAddress(argv.address);
